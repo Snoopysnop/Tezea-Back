21f4dec83b8838471b41eca872988f87c7d4b5e8
mfouillen <mathis.fouillen@etudiant.univ-rennes.fr>
2024-04-29 09:07:15 +0200

    Spring initlialized

---
+++ src/main/java/fr/isitc/tezea/TezeaApplication.java
@@ -0,0 +1,13 @@
+package fr.isitc.tezea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TezeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TezeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/isitc/tezea/TezeaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isitc.tezea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class tezeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
