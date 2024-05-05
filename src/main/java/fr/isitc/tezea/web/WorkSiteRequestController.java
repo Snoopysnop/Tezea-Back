c6175b3fcff87c39a5299796f164fb28f4fb3f8d
mfouillen <mathis.fouillen@univ-rennes1.fr>
2024-05-05 11:09:17 +0200

    Fixed import

--- src/main/java/fr/isitc/tezea/web/WorkSiteRequestController.java
+++ src/main/java/fr/isitc/tezea/web/WorkSiteRequestController.java
@@ -1,12 +1,12 @@
 package fr.isitc.tezea.web;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
