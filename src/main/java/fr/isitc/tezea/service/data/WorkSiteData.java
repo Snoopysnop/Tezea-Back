59d4749138a0e0edf1c3f6539b828c6303c3ccdc
Fouillen Mathis <mathis.fouillen@etudiant.univ-rennes1.fr>
2024-05-13 17:58:44 +0000

    Add category in worksites

--- src/main/java/fr/isitc/tezea/service/data/WorkSiteData.java
+++ src/main/java/fr/isitc/tezea/service/data/WorkSiteData.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import fr.isitc.tezea.model.ToolUsage;
 import fr.isitc.tezea.model.WorkSite;
+import fr.isitc.tezea.model.enums.Category;
 import fr.isitc.tezea.model.enums.SatisfactionLevel;
 import fr.isitc.tezea.model.enums.WorkSiteStatus;
 import fr.isitc.tezea.service.DTO.WorkSiteDTO;
@@ -20,6 +21,7 @@ public class WorkSiteData extends WorkSiteDTO {
     private WorkSiteStatus status;
     private String signature;
     private String comment;
+    private Category category;
 
     public WorkSiteData(WorkSite workSite, Set<ToolUsage> equipments) {
         super(workSite, equipments);
@@ -28,6 +30,7 @@ public class WorkSiteData extends WorkSiteDTO {
         this.status = workSite.getStatus();
         this.signature = workSite.getSignature();
         this.comment = workSite.getComment();
+        this.category = workSite.getRequest().getCategory();
 
     }
 }
